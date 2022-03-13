514970a549a0184289915c49a7b4c76c7a1b9363
jconnolly94 <jconnolly94@me.com>
2022-03-13 10:22:22 +0000

    Initial Commit

---
+++ ClassCreationPractical/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ClassCreationPractical
+//
+//  Created by James Connolly on 13/03/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
